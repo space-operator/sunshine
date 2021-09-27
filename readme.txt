d2596008b1a007229ae3ac5e89cd5beb343c47fa
enzotar <34943955+enzotar@users.noreply.github.com>
2021-09-27 11:36:33 -0700

    first commit

---
+++ readme.txt
@@ -0,0 +1,76 @@
+
+Rust Integrated Dart
+
+https://thlorenz.com/rid-site/
+
+
+Flutter side
+    Terminal like command line
+    Visualizer
+    Log similar film editing timeline
+        each line is one event
+
+
+
+Horizontal timeline
+    keyboard         create_node   name    ctrl    c              ctrl  v
+    mouse
+        right      x <ms x          
+        left                               x      xxx              xxx
+    
+
+
+    example1:
+        select a node, drag it, hold in above another for longer than 300ms, nest_node()
+        mouse
+            selected id      id123                 id456 300<x<800ms 
+            moving x,y          xxxxxxxxxxxxxxxxxxx
+        
+    
+
+match them against some pattern
+
+text input field / command
+    create_node (in the viz, it will a graph node)
+        enter parameter/arg: name: Amir
+    
+    move_node
+        provide node id 
+            x: 10
+            y: 50
+
+    move the coordinates of a node
+
+    copy
+
+
+if with mouse
+    I double click 
+        call the create_node function
+
+if keyboard control +c > copy()
+
+
+Language/protocol where everything work at command line first, then we map some to the events
+
+
+
+
+
+1. on Flutter side, I will capture all the events, mouse, keyboard, touch, etc, and send them to Rust side
+    Flutter side   input: stream of JSON, mock it
+
+
+2. Rust side
+    assume we have the input
+    
+    Process the steam
+    Parse
+    Match to a function 
+        eg. double_click()
+
+
+
+
+take the all the character/strings
+event when it can't be parsed
---
+++ src/app.rs
@@ -0,0 +1,88 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Default)]
+pub struct App {
+    pub events: Vec<Event>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Event {
+    KeyPressed(String),
+    KeyReleased(String),
+    MouseMove(u32, u32),
+    Error(String),
+}
+
+impl App {
+    pub fn push(&mut self, event: &str) {
+        let event = event.split_once(":").unwrap();
+        let event = match event.0 {
+            "p" => Event::KeyPressed(event.1.to_owned()),
+            "r" => Event::KeyReleased(event.1.to_owned()),
+            "m" => {
+                let coords = event.1.split_once(":").unwrap();
+                Event::MouseMove(coords.0.parse().unwrap(), coords.1.parse().unwrap())
+            }
+            _ => todo!(),
+        };
+        self.events.push(event);
+    }
+
+    pub fn how_much_spaces(&self) -> u32 {
+        let mut counter = 0;
+        let mut is_pressed = false;
+
+        for event in &self.events {
+            match event {
+                Event::KeyPressed(key) => {
+                    if key == "Space" {
+                        assert!(!is_pressed);
+                        is_pressed = true;
+                    }
+                }
+                Event::KeyReleased(key) => {
+                    if key == "Space" {
+                        assert!(is_pressed);
+                        is_pressed = false;
+                        counter += 1;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        counter
+    }
+}
+
+#[test]
+fn test_1() {
+    let data = r#" { "MouseMove": [123, 234] } "#;
+
+    let ev: Event = serde_json::from_str(data).unwrap();
+    panic!("{:?}", ev);
+}
+
+#[test]
+fn test_some_test() {
+    let mut app = App::default();
+
+    app.push("p:Space");
+    app.push("r:Space");
+    app.push("p:Escape");
+    app.push("r:Escape");
+    app.push("m:1234:1023");
+
+    assert_eq!(app.how_much_spaces(), 1);
+
+    assert_eq!(
+        app.events,
+        vec![
+            Event::KeyPressed("Space".to_owned()),
+            Event::KeyReleased("Space".to_owned()),
+            Event::KeyPressed("Escape".to_owned()),
+            Event::KeyReleased("Escape".to_owned()),
+            Event::MouseMove(1234, 1023),
+        ]
+    )
+}
