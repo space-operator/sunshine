8621fc131013f5ec950081606c7db1376384a044
Andrey Zheleznov <zheland.net@gmail.com>
2022-04-30 17:30:25 +0300

    InputCore: Remove unused code

--- input-core/src/unused/button.rs
+++
@@ -1,9 +0,0 @@
-use crate::{KeyboardKey, MouseButton, TouchId};
-
-#[allow(variant_size_differences)]
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub enum ButtonKind {
-    KeyboardKey(KeyboardKey),
-    MouseButton(MouseButton),
-    Touch(TouchId),
-}
--- input-core/src/unused/mouse.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub enum MouseButton {
-    Primary,
-    Secondary,
-    Auxiliary,
-    Other(u32),
-}
-
-pub type MouseScrollDelta = i32;
