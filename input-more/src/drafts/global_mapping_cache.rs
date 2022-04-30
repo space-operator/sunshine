0dc51a1dbdfae7f05aca2294771b4395c9b5ef8e
Andrey Zheleznov <zheland.net@gmail.com>
2022-04-30 17:43:25 +0300

    InputMore: Remove unused code

--- input-more/src/drafts/global_mapping_cache.rs
+++
@@ -1,7 +0,0 @@
-use crate::{DeviceMappingCache, ModifiersCache};
-
-#[derive(Clone, Debug)]
-pub struct GlobalMappingCache<Mo, KeSw, KeTr, KeEv> {
-    pub keyboard: DeviceMappingCache<KeSw, KeTr, Mo, KeEv>,
-    pub modifiers: ModifiersCache<Mo>,
-}
--- input-more/src/unused/struct_take_and_with_field.rs
+++
@@ -1,7 +0,0 @@
-#[macro_export]
-macro_rules! define_struct_take_and_with_field {
-    ( $( $tt:tt )* ) => {
-        $crate::define_struct_take_field!( $($tt) *);
-        $crate::define_struct_with_field!( $($tt) *);
-    };
-}
